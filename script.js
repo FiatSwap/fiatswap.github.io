6ac26ed714e66b6775ab78bab98f86820ce803a3
Not God <NotGod@zmail.com>
2021-03-13 13:38:28 +0530

    tronlink load wallet address

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
+const config = {
+	logTag: 'FIATSWAP - ',
+	tronGridApi: 'https://api.shasta.trongrid.io'
+};
+
 const app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-	$scope.favColor = 'Black';
+app.controller('myCtrl', function($scope, $window, $interval) {
+	$scope.state = {
+		loading: true
+	};
+	$scope.account = {
+		address: null
+	};
+	
+	
+	// on UI loaded
+	$window.onload = function(e) {
+		if ($window.tronWeb) {
+			let tries = 0, totalTries = 15;
+			const interval = $interval(function() {
+				if ($window.tronWeb.defaultAddress.base58 != false) {
+					$scope.account.address = {
+						long: $window.tronWeb.defaultAddress.base58,
+						short: formatAddress($window.tronWeb.defaultAddress.base58)
+					}
+
+					
+					$interval.cancel(interval);
+					return;
+				}
+
+				tries++;
+				if (tries > totalTries) $interval.cancel(interval);
+			}, 1000);
+		} else {
+			alert('Install TronLink to continue');
+		}
+	}
 });
+
+// format address
+function formatAddress(address) {
+	const start = address.substr(0, 4);
+	const end = address.substr(address.length - 4);
+	return `${start}...${end}`;
+}
